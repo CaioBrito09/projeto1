a80447e111ab479e1c4d6516a72ef8bffea53d29
CaioBrito09 <caio.debrito@dcomp.ufs.br>
2024-10-14 23:39:56 -0300

    Add files via upload

---
+++ projeto1/code.js
@@ -0,0 +1,17 @@
+const muda = document.querySelector('.anima');
+
+
+function start() {
+    window.location.href = "game.html";
+}
+
+
+function testa(){
+    const formulaEnviada = document.getElementById("form").value
+    if(formulaEnviada=="resp1"){
+        window.location.href="index.html";
+    }
+
+}
+
+
